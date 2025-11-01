d20d369260719a2607df7e898e4dfba003c6db72
ravihoro <ravijohnhoro5@gmail.com>
2025-11-01 14:44:04 +0530

    Added home screen

---
+++ app/src/main/java/com/example/ecom/presentation/ui/home/HomeScreen.kt
@@ -0,0 +1,51 @@
+package com.example.ecom.presentation.ui.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.ecom.presentation.viewmodel.ProductListViewModel
+import com.example.ecom.presentation.viewmodel.UiState
+
+@Composable
+fun HomeScreen(
+    viewModel: ProductListViewModel = hiltViewModel()
+) {
+
+    val state by viewModel.uiState.collectAsState()
+
+    when(val s = state){
+        is UiState.Loading -> Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator()
+        }
+
+        is UiState.Error -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "Error: ${s.message}")
+            }
+        }
+
+        is UiState.Success -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ){
+                Text(text = "Data loaded")
+            }
+        }
+    }
+
+}
